5dbd661bf677c4d69e9e5016b009d46cbf0b16f3
BananaLeaf77 <dognub61@gmail.com>
2025-10-21 14:10:33 +0800

    update

--- delivery/teacher.go
+++ delivery/teacher.go
@@ -5,6 +5,7 @@ import (
 	"chronosphere/domain"
 	"chronosphere/middleware"
 	"chronosphere/utils"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -28,11 +29,37 @@ func NewTeacherHandler(app *gin.Engine, tc domain.TeacherUseCase, jwtManager *ut
 }
 
 func (th *TeacherHandler) GetMyProfile(c *gin.Context) {
+	name := utils.GetAPIHitter(c)
+	userUUID, exists := c.Get("userUUID")
+	if !exists {
+		utils.PrintLogInfo(&name, 401, "GetMyProfile", nil)
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"success": false,
+			"message": "Unauthorized: missing user context",
+		})
+		return
+	}
 
+	// Call usecase to get teacher data
+	user, err := th.tc.GetMyProfile(c.Request.Context(), userUUID.(string))
+	if err != nil {
+		utils.PrintLogInfo(&name, 500, "GetMyProfile", &err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	utils.PrintLogInfo(&name, 200, "GetMyProfile", nil)
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data":    user,
+	})
 }
 
 func (th *TeacherHandler) UpdateTeacherData(c *gin.Context) {
-
+	
 }
 
 func (th *TeacherHandler) AddAvailability(c *gin.Context) {
