37679ad0cf76a8d571ad8215721eaf47154878fb
BananaLeaf77 <dognub61@gmail.com>
2025-12-17 21:30:18 +0800

    update

--- dto/admin_dto.go
+++ dto/admin_dto.go
@@ -4,7 +4,6 @@ import "chronosphere/domain"
 
 type UpdateAdminProfileRequest struct {
 	Name   string `json:"name" binding:"required,min=3,max=50"`
-	Email  string `json:"email" binding:"required,email"`
 	Phone  string `json:"phone" binding:"required,numeric,min=9,max=14"`
 	Image  string `json:"image" binding:"omitempty,url"`
 	Gender string `json:"gender" binding:"required,oneof=male female"`
@@ -13,7 +12,6 @@ type UpdateAdminProfileRequest struct {
 func MakeUpdateAdminProfileRequest(req *UpdateAdminProfileRequest) domain.User {
 	return domain.User{
 		Name:   req.Name,
-		Email:  req.Email,
 		Phone:  req.Phone,
 		Image:  &req.Image,
 		Gender: req.Gender,
